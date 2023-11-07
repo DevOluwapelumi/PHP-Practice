2073c1da85eb955869ef23740f3dd8758726469e
OGUNDIRAN VICTOR ISREAL <victoroluwadarasimi16@gmail.com>
2023-11-07 13:39:18 +0100

    tailwindcss styling commit

--- signup.php
+++ signup.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
     <title>SignUps Page</title>
 </head>
-<body>
+<body class="bg-gray-100">
 
-<div class="container">
+<div class="container mx-auto p-6">
     <div>
         <div>
             <?php
@@ -20,20 +21,20 @@
             ?>
         </div>
     </div>
-        <div>
-            <form action="submit.php" method="post">
-                <input type="text" placeholder="ProductName" name="ProductName">
-                <input type="text" placeholder="Description" name="Description">
-                <input type="text" placeholder="Category" name="Category">
-                <input type="number" placeholder="Price" name="Price">
-                <input type="number" placeholder="StockQuantity" name="StockQuantity">
-                <input type="text" placeholder="Manufacturer" name="Manufacturer">
-                <input type="text" placeholder="Supplier" name="Supplier">
-                <input type="text" placeholder="Barcode" name="Barcode">
-                <input type="date" placeholder="ReleaseDate" name="ReleaseDate">
-                <button type="submit" value="submit" name="submit">Submit</button>
-            </form>
-        </div>
+    <div class="max-w-md mx-auto mt-6 bg-white p-6 rounded-lg shadow-md">
+        <form action="submit.php" method="post" class="space-y-4">
+            <input type="text" placeholder="Product Name" name="ProductName" class="w-full border rounded-lg px-3 py-2">
+            <input type="text" placeholder="Description" name="Description" class="w-full border rounded-lg px-3 py-2">
+            <input type="text" placeholder="Category" name "Category" class="w-full border rounded-lg px-3 py-2">
+            <input type="number" placeholder="Price" name="Price" class="w-full border rounded-lg px-3 py-2">
+            <input type="number" placeholder="Stock Quantity" name="StockQuantity" class="w-full border rounded-lg px-3 py-2">
+            <input type="text" placeholder="Manufacturer" name="Manufacturer" class="w-full border rounded-lg px-3 py-2">
+            <input type="text" placeholder="Supplier" name="Supplier" class="w-full border rounded-lg px-3 py-2">
+            <input type="text" placeholder="Barcode" name="Barcode" class="w-full border rounded-lg px-3 py-2">
+            <input type="date" placeholder="Release Date" name="ReleaseDate" class="w-full border rounded-lg px-3 py-2">
+            <button type="submit" value="submit" name="submit" class="w-full bg-green-500 text-white font-semibold rounded-lg px-4 py-2">Submit</button>
+        </form>
+    </div>
     </div>
     
 </body>
